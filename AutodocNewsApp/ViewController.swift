0151f0f33680137bdd111798f30556ed63c73d4f
Aleksey Chaykin <barjastroy@gmail.com>
2026-07-02 08:42:09 +0300

    Initial Commit

---
+++ AutodocNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutodocNewsApp
+//
+//  Created by A Ch on 02.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutodocNewsAppTests/AutodocNewsAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  AutodocNewsAppTests.swift
+//  AutodocNewsAppTests
+//
+//  Created by A Ch on 02.07.2026.
+//
+
+import Testing
+@testable import AutodocNewsApp
+
+struct AutodocNewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
